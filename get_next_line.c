8b6f92f8d866347550cab5edcd59b237928c5ecd
4bderahim <abderahimelkridah@gmail.com>
2024-01-15 17:38:51 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -5,23 +5,28 @@
 // #define 
 // #endif
 
-size_t	ft_strlcat_next_line(char *dst, const char *src, char c)//, size_t dstsize)
+size_t	ft_strlcat_next_line(char *dst, const char *src, int c)//, size_t dstsize)
 {
 	size_t	dstlen;
 	size_t	srclen;
 	size_t	i;
 	size_t	dstlen_increased;
 
-	dstlen = strlen(dst);
-	srclen = strlen(src);
-	dstlen_increased = dstlen;
+	//dstlen = strlen(dst);
+	//srclen = strlen(src);
+	dstlen_increased = c;
 	i = 0;
-	while (src[i] || src[i] != c)
+	while (src[i])
 	{
-		    dst[dstlen_increased] = src[i];
-		    i++;
-		    dstlen_increased++;
-            printf("%zu", i);
+        printf("noNE");
+        
+       // if (i == dstlen_increased )
+		//    {
+                
+                dst[dstlen_increased] = src[i];
+		        i++;
+		        dstlen_increased++;
+          //  }
 	}
 	dst[dstlen_increased] = '\0';
 	return (dstlen + srclen);
@@ -54,61 +59,15 @@ char	*ft_strjoin(char *s1, char *s2,int n)
 	allocated = (char *)malloc(string_len + 1);
 	if (!allocated)
 		return (NULL);
-    printf("{{%s}}", jj);
+    //printf("{{%s}}", j);
 	join_the_two_strings(allocated, s1, s2,n);
 	allocated[string_len] = '\0';
 	return (allocated);
 }
 
-// void buf_pocket(char *buf, char *pocket, int length)
-// {
-    
-// }
 char *get_next_line(int fd)
 {
-    char *buf;
-    char *tmp;
-    static char *pocket;
-    int i;
-    int BUFFER_SIZE  = 12;
-    char *ret;
-    ssize_t cc;
-    d\nf\d\ndgn\n
-    i = 1;
-    cc = 1;
-    buf = NULL;
-    while (cc != 0)
-        {
-            if (pocket != NULL)
-            {
-                ret = strchr(pocket, '\n');
-                pocket = (char *)realloc(pocket , (BUFFER_SIZE*i)+1);
-                pocket[BUFFER_SIZE*i] = 0;
-                if (ret)
-                    {
-                           buf  = ft_strjoin("", pocket, (((int) ret)-((int) pocket))+1);
-                           return (buf);
-                    } 
-                   
-            }
-            pocket = (char *)realloc(pocket , (BUFFER_SIZE*i)+1);
-            pocket[BUFFER_SIZE*i] = 0;
-
-            cc = read(fd, pocket, BUFFER_SIZE);
-            //printf("[%s]\n", pocket);
-            ret = strchr(pocket, '\n');
-            if ((ret))
-            {
-             //   printf("[[%s||%d||%s",ret, ((int) ret)-((int) pocket),pocket);
-                buf  = ft_strjoin("", pocket,(((int) ret)-((int) pocket))+1);
-                printf("??%s\n", buf);
-                strncpy(pocket, ret, strlen(ret));
-                printf("\n\n\t%s", pocket);
-                break;
-            }
-            i++;
-        }
-    return (buf);
+    
 }
 
 
@@ -148,53 +107,48 @@ char *get_next_line(int fd)
 //     i = 1;
 //     cc = 1;
 //     buf = NULL;
-//     tmp = NULL;
 //     while (cc != 0)
 //         {
-//             tmp = (char *)realloc(tmp , (BUFFER_SIZE*i)+1);
-//             buf = (char *)realloc(buf , (BUFFER_SIZE*i)+1);
-//             tmp[BUFFER_SIZE*i] = 0;
-//             buf[BUFFER_SIZE*i] = 0;
-//             cc = read(fd, tmp, BUFFER_SIZE);
-             
-//             ret = strchr(tmp, '\n');
-          
-//             if (pocket != NULL)  
+//            // printf("\t\t\t{{%s}}\n", pocket);
+//             if (pocket != NULL)
 //             {
-//                 strcpy(buf, pocket);
-//                 printf("##%s||%s\n", pocket, buf);
-
-//             }
-             
-//             if ((ret) && tmp[0] != 0)
-//             {
-//                 if (ret[1] != '\0')
+                
+//                 ret = strchr(pocket, '\n');
+//                 pocket = (char *)realloc(pocket , (BUFFER_SIZE*i)+1);
+//                 pocket[BUFFER_SIZE*i] = 0;
+//                 if (ret)
 //                     {
-//                         pocket = (char *) realloc(pocket, strlen(ret)+1);
-//                         pocket[strlen(ret)] = 0;
-//                         // printf("{{{%s}}}\n", pocket);
-//                         strlcat(pocket, ret+1, strlen(pocket));
-//                         //printf("[[%s]]\n", );
-//                         //strlcat(pocket, ret, strlen(pocket)); 
-//                     }
-//                 int i = 0;
-//                 while(tmp[i] !='\n')//tmp != ret)
-//                 {
-//                     buf[i] = tmp[i];
-//                     i++;
-//                 }
+                        
+//                            buf  = ft_strjoin("", pocket, (((int) ret)-((int) pocket))+1);
+//                            return (buf);
+//                     } 
+//               //  buf = ft_strjoin("", pocket, strlen(pocket));
+//                // pocket = NULL;
+//                 //return (buf);
+//             }
+            
+//             pocket = (char *)realloc(pocket , (BUFFER_SIZE*i)+1);
+//             pocket[BUFFER_SIZE*i] = 0;
+//             tmp = (char *)realloc(tmp , (BUFFER_SIZE)+1);
+//             tmp[BUFFER_SIZE] = 0;
+            
+//             cc = read(fd, tmp, BUFFER_SIZE);
+//             ft_strlcat_next_line(pocket, tmp, BUFFER_SIZE*i);
+//             free(tmp);
+//             printf("%s", pocket);
+
+//             printf("\t\t\t\t%lu\t-%s-\n", strlen(pocket),pocket);
+//             ret = strchr(pocket, '\n');
+//             printf("%s||||%lu\n\n", pocket, strlen(pocket));
+//             if ((ret))
+//             {
+//                 buf  = ft_strjoin("", pocket,(((int) ret)-((int) pocket))+1);
+//                // printf("#%s", buf);
+//                 strncpy(pocket, ret+1, strlen(pocket));
+                
 //                 break;
 //             }
 //             i++;
 //         }
-//     // if (cc == 0)
-//     //     return (0);
-//     //if (cc == 0)
-//     printf("\t\t\t[%s||%s]\n", buf, pocket);
-//     //free(pocket);
-//     free(tmp);
-    
 //     return (buf);
 // }
-
-
