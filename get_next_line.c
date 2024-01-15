3d0c019cab4e8184108434649c5674d120d781b1
4bderahim <abderahimelkridah@gmail.com>
2024-01-15 23:47:36 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -5,33 +5,27 @@
 // #define 
 // #endif
 
-size_t	ft_strlcat_next_line(char *dst, const char *src, int c)//, size_t dstsize)
+size_t	ft_strlcat_next_line(char *dst, const char *src)//, size_t dstsize)
 {
 	size_t	dstlen;
 	size_t	srclen;
 	size_t	i;
 	size_t	dstlen_increased;
 
-	//dstlen = strlen(dst);
-	//srclen = strlen(src);
-	dstlen_increased = c;
+	dstlen = strlen(dst);
+	srclen = strlen(src);
+	dstlen_increased = 0;
 	i = 0;
 	while (src[i])
 	{
-        printf("noNE");
-        
-       // if (i == dstlen_increased )
-		//    {
-                
                 dst[dstlen_increased] = src[i];
 		        i++;
 		        dstlen_increased++;
-          //  }
 	}
 	dst[dstlen_increased] = '\0';
 	return (dstlen + srclen);
 }
-static void	join_the_two_strings(char *all, char const *s1, char const *s2, int n)
+static void	join_the_two_strings(char *all, char const *s1, char const *s2)
 {
 	size_t	i;
 	int		j;
@@ -43,7 +37,7 @@ static void	join_the_two_strings(char *all, char const *s1, char const *s2, int 
 		i++;
 	}
 	j = 0;
-	while (s2[j] && j < n)
+	while (s2[j])
 	{
 		all[i] = s2[j];
 		i++;
@@ -51,23 +45,71 @@ static void	join_the_two_strings(char *all, char const *s1, char const *s2, int 
 	}
 }
 
-char	*ft_strjoin(char *s1, char *s2,int n)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*allocated;
 	size_t	string_len;
-	string_len = strlen(s1) + strlen(s2);
+	int x;
+	x = 0;
+	if (s1 != NULL)
+		x = strlen(s1);
+	string_len = x + strlen(s2);
 	allocated = (char *)malloc(string_len + 1);
 	if (!allocated)
 		return (NULL);
     //printf("{{%s}}", j);
-	join_the_two_strings(allocated, s1, s2,n);
+	join_the_two_strings(allocated, s1, s2);
 	allocated[string_len] = '\0';
 	return (allocated);
 }
 
+char *line_join(char *pocket, int fd)
+{
+	char *buf;
+	int cc;
+	char *check;
+	int BUFFER_SIZE = 12;
+
+	buf = (char *) calloc(1, BUFFER_SIZE+1);
+	buf[BUFFER_SIZE] = 0;
+	while (cc != 0)
+	{
+		cc = read(fd ,buf, BUFFER_SIZE);
+		pocket = ft_strjoin(pocket, buf);
+		check = strchr(buf, '\n');
+		if (check)
+		{
+			free(buf);
+			return (pocket);
+		}
+	}
+	free(buf);
+	return (pocket);
+    }
+
 char *get_next_line(int fd)
 {
-    
+	static char *pocket;
+	char *new_line_check;
+	char *buffer;
+	int i;
+    pocket  = line_join(pocket, fd);
+	new_line_check = strchr(pocket, '\n');
+	if (new_line_check)
+	{
+		if (new_line_check[1] == '\0')
+			{
+				buffer = pocket;
+				buffer[strlen(pocket)-1] = '\0';
+				free(pocket);
+			}
+		else
+		{
+			free(pocket);
+			pocket = ft_strjoin("", new_line_check);
+		}
+	}
+    return (buffer);
 }
 
 
