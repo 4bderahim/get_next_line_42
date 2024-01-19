43174ef3472ca5d86dc178132859db0e5bce902a
4bderahim <abderahimelkridah@gmail.com>
2024-01-19 23:55:12 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,5 @@
+
+
 #include "get_next_line.h"
 
 static void	join_the_two_strings(char *all, char const *s1, char const *s2)
@@ -6,7 +8,6 @@ static void	join_the_two_strings(char *all, char const *s1, char const *s2)
 	int		j;
 
 	i = 0;
-	
 	if (s1 !=0)
 	{
 		while (s1[i])
@@ -24,54 +25,228 @@ static void	join_the_two_strings(char *all, char const *s1, char const *s2)
 	}
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2, int one_to_free)
 {
-	
 	char	*allocated;
 	size_t	string_len;
 	int x;
 	x = 0;
 	if (s1 != NULL)
 		{
-			
-			x = strlen(s1);
+			x = ft_strlen(s1);
 		}
-	
 	string_len = x + strlen(s2);
 	allocated = (char *)malloc(string_len + 1);
 	if (!allocated)
 		return (NULL);
 	join_the_two_strings(allocated, s1, s2);
 	allocated[string_len] = '\0';
+	if (one_to_free == 1)
+		free(s1);
 	return (allocated);
 }
+
 char *line_join(char *pocket, int fd)
 {
 	
 	char *buf;
 	int cc;
 	char *check;
-	char *tmp;
-	buf = (char *) calloc(1, BUFFER_SIZE+1);
+	int i;
+
+	i = 0;
 	cc = 1;
+	
 	while (cc != 0)
 	{
+        buf = (char *) calloc(1, BUFFER_SIZE+1);
+       
 		cc = read(fd ,buf, BUFFER_SIZE);
-		pocket = ft_strjoin(pocket, buf);
-		tmp = pocket;
-		free(pocket);
-		check = ft_strchr(buf, '\n');
+        //printf("\t{{%s|%d}}\n", buf, cc);
+         //printf("!!!!%s|%d|%s!!!!\n", buf,i , pocket);
+         //--------\/
+        if (cc == 0 && i == 0)
+            return (ft_strjoin("", "",0));
+        
+		pocket = ft_strjoin(pocket, buf, 1);
+		//printf("[[+++%s+++]]\n", pocket);
+		
+		check = ft_strchr(pocket, '\n');//|| ft_strchr(buf, '\0') != NULL
+		//printf("[[%s||%s||%s||%d]]\n", check, buf, pocket, cc);
 		if (check)
 		{
 			free(buf);
 			return (pocket);
 		}
 		if (cc == -1)
-			return (NULL);
+			{
+				free(buf);
+				return (NULL);
+			}
+        i++;
+        free(buf);
 	}
-	free(buf);
+    //printf("##%s##\n", pocket);
 	return (pocket);
 }
+char *souvernire(char *pocket,char *rest)
+{
+	char *buf;
+
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	if (pocket == NULL)
+		return (NULL);
+	if(rest[1] == '\0' || pocket[0] == '\0')
+		{
+            return (pocket);
+			// free(pocket);
+			// return (ft_strjoin("","",0));
+		}
+	rest++;
+	buf = ft_strjoin("", rest, 0);
+	rest--;
+	free(pocket);
+	return (buf);
+}
+char *pocket_change(char *pocket, char *reserve)
+{
+	char *buf;
+	int i;
+
+	i = 0;
+	buf = ft_calloc((((long) reserve)-((long) pocket))+1, 1);
+
+	while (pocket[i-1] != '\n')
+	{
+		buf[i] = pocket[i];
+		i++;
+	}
+	return (buf);
+}
+char *first_line(char *rest)
+{
+	char *buf;
+
+	int i;
+	i = 1;
+	buf = (char *) calloc(strlen(rest), 1);
+	while(rest[i])
+	{
+		*buf = rest[i];
+		i++;
+		buf++;
+	}
+	return (buf);
+}
+char *get_next_line(int fd)
+{
+	static char *pocket;
+	char *new_line_check;
+	char *buffer;
+
+	if (fd <= 0 || fd == 1 || fd == 2 || fd > 65535)
+		{
+           // free(pocket);
+            //pocket = NULL;
+            return(NULL);
+        }
+	// if (pocket != NULL)
+    // {
+    //   //  printf("@@%s@@\n", pocket);
+        
+    //     if (pocket[0] == '\0')
+    //     {
+    //         pocket = NULL;
+    //         free(pocket);
+    //         return (ft_strjoin("", "", 1));
+    //     }
+    // }
+    pocket  = line_join(pocket, fd);
+   // printf("\t=%s=\n", pocket);
+	if (pocket == NULL)
+		return (NULL);
+   // printf("\t\t\t((%s}\n", pocket);
+	new_line_check = ft_strchr(pocket, '\n');
+	if (new_line_check)
+		{
+            
+			buffer = pocket_change(pocket, new_line_check);
+            
+			// buffer = ft_strjoin(buffer, "", 1);
+			//printf("\t\t\t\t\t\t\t[%s]\n", buffer);
+		}
+	else{
+		buffer = ft_strjoin(pocket, "", 1);
+       // printf("\t\t\t{%s}\n", buffer);
+		pocket = NULL;
+		return buffer;
+	}
+	pocket = souvernire(pocket, new_line_check);
+	
+	return (buffer);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// static void	join_the_two_strings(char *all, char const *s1, char const *s2)
+// {
+// 	size_t	i;
+// 	int		j;
+
+// 	i = 0;
+// 	if (s1 !=0)
+// 	{
+// 		while (s1[i])
+// 		{
+// 			all[i] = s1[i];
+// 			i++;
+// 		}
+// 	}
+// 	j = 0;
+// 	while (s2[j])
+// 	{
+// 		all[i] = s2[j];
+// 		i++;
+// 		j++;
+// 	}
+// }
+
+// char	*ft_strjoin(char *s1, char *s2, int one_to_free)
+// {
+	
+// 	char	*allocated;
+// 	size_t	string_len;
+// 	int x;
+// 	x = 0;
+// 	if (s1 != NULL)
+// 		{
+// 			x = ft_strlen(s1);
+// 		}
+// 	string_len = x + strlen(s2);
+// 	allocated = (char *)malloc(string_len + 1);
+// 	if (!allocated)
+// 		return (NULL);
+// 	join_the_two_strings(allocated, s1, s2);
+// 	allocated[string_len] = '\0';
+// 	if (one_to_free == 1)
+// 		free(s1);
+// 	return (allocated);
+// }
 
 // char *line_join(char *pocket, int fd)
 // {
@@ -79,140 +254,140 @@ char *line_join(char *pocket, int fd)
 // 	char *buf;
 // 	int cc;
 // 	char *check;
-
-// 	buf = (char *) calloc(1, BUFFER_SIZE+1);
+	
+	
 // 	cc = 1;
+// 	buf = (char *) calloc(1, BUFFER_SIZE+1);
 // 	while (cc != 0)
 // 	{
+		
 // 		cc = read(fd ,buf, BUFFER_SIZE);
-// 		pocket = ft_strjoin(pocket, buf);
-// 		check = ft_strchr(buf, '\n');
+// 		//printf("\t\t\t+#%s||%zu#+\n",buf, ft_strlen(buf) );
+// 		pocket = ft_strjoin(pocket, buf, 1);
+// 		//if (*pocket == '\0')
+		
+// 		//printf("\n\n$$$$[%s]$$$$\n\n", pocket);
+// 		check = ft_strchr(pocket, '\n');//|| ft_strchr(buf, '\0') != NULL
+// 		//printf("[[%s||%s||%s||%d]]\n", check, buf, pocket, cc);
 // 		if (check)
 // 		{
 // 			free(buf);
 // 			return (pocket);
 // 		}
 // 		if (cc == -1)
-// 			return (NULL);
+// 			{
+// 				free(buf);
+// 				return (NULL);
+// 			}
+		
 // 	}
 // 	free(buf);
+	
 // 	return (pocket);
-//     }
-char *get_next_line(int fd)
-{
-	static char *pocket;
-	char *new_line_check;
-	char *buffer;
-	int i;
-    pocket  = line_join(pocket, fd);
-	if (fd < 0)
-		return(NULL);
-	if (*pocket == '\0')
-				return (NULL);
-	new_line_check = ft_strchr(pocket, '\n');
-	if (new_line_check)
-	{
-		if (new_line_check[1] == '\0')
-			{
-				buffer = pocket;
-				buffer = ft_strjoin("", pocket);
-				buffer[ft_strlen(pocket)-1] = 0;
-				pocket = NULL;
-				free(pocket);
-			}
-		else
-		{
-			buffer = (char *) ft_calloc(1, (((long) new_line_check)-((long) pocket))+1);
-			i =0;
-			while(pocket[i] != '\n')
-			{
-				buffer[i] = pocket[i];
-				i++;
-			}
-			pocket = ft_strjoin("", new_line_check+1);
-		}
-		return (buffer);
-	}
-    return (NULL);
-}
+// }
+// char *souvernire(char *pocket,char *rest)
+// {
+// 	char *buf;
 
+// 	int i;
+// 	int j;
 
+// 	j = 0;
+// 	i = 0;
+// 	if (pocket == NULL)
+// 		return (NULL);
+// 	if(rest[1] == '\0' || pocket[0] == '\0')
+// 		{
+// 			free(pocket);
+// 			return (ft_calloc(0,0));
+// 		}
+// 	//printf("\t!!%s||%ld!!\n", rest, (((long) rest)-((long) pocket)));
+// 	//rest++;
+// 	buf = ft_strjoin("", pocket+(((long) rest)-((long) pocket))+1, 0);///////////
+// 	//rest--;
+// 	free(pocket);
+// 	return (buf);
+// }
+// char *pocket_change(char *pocket, char *reserve)
+// {
+// 	char *buf;
+// 	int i;
 
+// 	i = 0;
+// 	if (!pocket[0])
+// 		return (NULL);
+// 	buf = ft_calloc((((long) reserve)-((long) pocket))+1, 1);
 
+// 	while (pocket[i-1] != '\n')
+// 	{
+// 		buf[i] = pocket[i];
+// 		i++;
+// 	}
+// 	//printf("\t{{%s}}\n", buf);
+// 	return (buf);
+// }
+// char *first_line(char *rest)
+// {
+// 	char *buf;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 	int i;
+// 	i = 1;
+// 	buf = (char *) calloc(strlen(rest), 1);
+// 	while(rest[i])
+// 	{
+// 		*buf = rest[i];
+// 		i++;
+// 		buf++;
+// 	}
+// 	return (buf);
+// }
 
 // char *get_next_line(int fd)
 // {
-//     char *buf;
-//     char *tmp;
-//     static char *pocket;
-//     int i;
-//     int BUFFER_SIZE  = 12;
-//     char *ret;
-//     ssize_t cc;
-//     i = 1;
-//     cc = 1;
-//     buf = NULL;
-//     while (cc != 0)
-//         {
-//            // printf("\t\t\t{{%s}}\n", pocket);
-//             if (pocket != NULL)
-//             {
-                
-//                 ret = strchr(pocket, '\n');
-//                 pocket = (char *)realloc(pocket , (BUFFER_SIZE*i)+1);
-//                 pocket[BUFFER_SIZE*i] = 0;
-//                 if (ret)
-//                     {
-                        
-//                            buf  = ft_strjoin("", pocket, (((int) ret)-((int) pocket))+1);
-//                            return (buf);
-//                     } 
-//               //  buf = ft_strjoin("", pocket, strlen(pocket));
-//                // pocket = NULL;
-//                 //return (buf);
-//             }
-            
-//             pocket = (char *)realloc(pocket , (BUFFER_SIZE*i)+1);
-//             pocket[BUFFER_SIZE*i] = 0;
-//             tmp = (char *)realloc(tmp , (BUFFER_SIZE)+1);
-//             tmp[BUFFER_SIZE] = 0;
-            
-//             cc = read(fd, tmp, BUFFER_SIZE);
-//             ft_strlcat_next_line(pocket, tmp, BUFFER_SIZE*i);
-//             free(tmp);
-//             printf("%s", pocket);
-
-//             printf("\t\t\t\t%lu\t-%s-\n", strlen(pocket),pocket);
-//             ret = strchr(pocket, '\n');
-//             printf("%s||||%lu\n\n", pocket, strlen(pocket));
-//             if ((ret))
-//             {
-//                 buf  = ft_strjoin("", pocket,(((int) ret)-((int) pocket))+1);
-//                // printf("#%s", buf);
-//                 strncpy(pocket, ret+1, strlen(pocket));
-                
-//                 break;
-//             }
-//             i++;
-//         }
-//     return (buf);
+// 	static char *pocket;
+// 	char *new_line_check;
+// 	char *buffer;
+// 	if (fd < 0)
+// 		return(NULL);
+// 	//if (!pocket)
+// 	//	pocket = ft_calloc(0,0);
+//     pocket  = line_join(pocket, fd);
+// 	//printf("\t\t\t\t{{%s}}\n", pocket);
+// 	if (pocket[0] == '\n' && pocket[1] == '\0')
+// 		{
+			
+// 			return (ft_strjoin("","\n", 0));
+// 		}
+// 	if (pocket[0] == '\0')
+// 		{
+// 			return (NULL);
+// 		}
+// 	//printf("\t\t\t[%s]\n", pocket);
+// 	if (pocket == NULL)
+// 		return (NULL);
+// 	new_line_check = ft_strchr(pocket, '\n');
+// 	if (new_line_check)
+// 		{
+// 			buffer = pocket_change(pocket, new_line_check);
+// 			// buffer = ft_strjoin(buffer, "", 1);
+// 			//printf("\t\t\t\t\t\t\t[%s]\n", buffer);
+// 		}
+// 	else{
+// 		buffer = ft_strjoin(pocket, "", 1);
+// 		pocket = NULL;
+// 		return buffer;
+// 	}
+// 	pocket = souvernire(pocket, new_line_check);
+// 	//printf("#%s#\n", pocket);
+// 	return (buffer);
 // }
+
+
+
+
+
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int main()
+{
+
+    int t = open("he", O_RDONLY);
+    //int d = 0;
+    char *s1 = get_next_line(t);
+    char *s2 = get_next_line(t);
+     char *s3 = get_next_line(t);
+    // char *s4 = get_next_line(t);
+    // char *s5 = get_next_line(t);
+    printf("[%s]", s1);
+    printf("\n\n======\n\n");
+    printf("[%s]", s2);
+     printf("[%s]", s3);
+    // printf("%s", s4);
+    // printf("%s", s5);
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+//    printf("=%s=\n", get_next_line(t));
+   //system("leaks -q a.out");
+    close(t);
+    //printf("[%d}[%s]",i ,buf);
+}
